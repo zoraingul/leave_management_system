fdaaf943306317b0b4dac733304dc8907e8c6cc6
zoraingul <zorain.stu@gmail.com>
2026-04-29 22:45:37 +0500

    update ui and add leave approval

--- script.js
+++ script.js
@@ -1,51 +1,103 @@
 let leaves = JSON.parse(localStorage.getItem("leaves")) || [];
 
+
+function saveLeaves() {
+  localStorage.setItem("leaves", JSON.stringify(leaves));
+}
+
+// apply leave
 const form = document.getElementById("leaveForm");
 
 if (form) {
   form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const newLeave = {
+    const leave = {
       name: document.getElementById("name").value,
       dept: document.getElementById("dept").value,
       type: document.getElementById("type").value,
       from: document.getElementById("from").value,
       to: document.getElementById("to").value,
+      reason: document.getElementById("reason").value,
       status: "Pending"
     };
 
-    leaves.push(newLeave);
-    localStorage.setItem("leaves", JSON.stringify(leaves));
+    leaves.push(leave);
 
-    alert("Leave Submitted Successfully!");
+    saveLeaves();
+
+    alert("Leave submitted successfully!");
+
     form.reset();
   });
 }
 
-// history page
+// history
 const table = document.getElementById("historyTable");
 
 if (table) {
-  leaves.forEach(l => {
+
+  leaves.forEach((leave, index) => {
+
+    let statusClass = leave.status.toLowerCase();
+
     table.innerHTML += `
       <tr>
-        <td>${l.name}</td>
-        <td>${l.type}</td>
-        <td>${l.from} - ${l.to}</td>
-        <td>${l.status}</td>
+        <td>${leave.name}</td>
+        <td>${leave.dept}</td>
+        <td>${leave.type}</td>
+        <td>${leave.from} to ${leave.to}</td>
+
+        <td>
+          <span class="status ${statusClass}">
+            ${leave.status}
+          </span>
+        </td>
+
+        <td>
+          <button onclick="approveLeave(${index})">
+            Approve
+          </button>
+
+          <button onclick="rejectLeave(${index})"
+            style="background:red; margin-left:5px;">
+            Reject
+          </button>
+        </td>
       </tr>
     `;
   });
 }
 
-//dashboard stats
+function approveLeave(index) {
+
+  leaves[index].status = "Approved";
+
+  saveLeaves();
+
+  location.reload();
+}
+
+function rejectLeave(index) {
+
+  leaves[index].status = "Rejected";
+
+  saveLeaves();
+
+  location.reload();
+}
+
 const total = document.getElementById("total");
 const pending = document.getElementById("pending");
 const approved = document.getElementById("approved");
 
 if (total) {
+
   total.innerText = leaves.length;
-  pending.innerText = leaves.filter(l => l.status === "Pending").length;
-  approved.innerText = leaves.filter(l => l.status === "Approved").length;
+
+  pending.innerText =
+    leaves.filter(l => l.status === "Pending").length;
+
+  approved.innerText =
+    leaves.filter(l => l.status === "Approved").length;
 }
